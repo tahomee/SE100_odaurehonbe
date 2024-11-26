5f073881e9e4e17b53a29b9fa0c756b396503535
tahomee <thaothanh6g@gmail.com>
2024-11-26 19:42:12 +0700

    Set up PostgreSQL

---
+++ odaurehonbe/Data/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace PostgreSQL.Data
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public char? Gender { get; set; } 
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; } 
+        public string Address { get; set; } 
+    }
+}
